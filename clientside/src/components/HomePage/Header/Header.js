00de1e0f18af4dba6ba3bf2e55c4cdb446b5bf34
vikrant <vikrant.negi74@gmail.com>
2021-06-11 22:15:55 +0530

    Header Added

---
+++ clientside/src/components/HomePage/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="User__profileImage" />
+            <div className="User__profileName">Vikrant Negi</div>
+            <div className="User_profileTitle">Welcome to my Blogs.</div>            
+        </header>
+    )
+}
