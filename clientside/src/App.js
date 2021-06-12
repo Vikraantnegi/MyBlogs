830881da5f2faf6801557e3ff3604616163d812f
vikrant <vikrant.negi74@gmail.com>
2021-06-12 10:27:21 +0530

    Routes added

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import HomePage from './screens/HomePage/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/blog">
+            <BlogPage />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
