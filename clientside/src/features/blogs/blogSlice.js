0f2292955f16b6a74bb1b3fb930af5e9442c998a
vikrant <vikrant.negi74@gmail.com>
2021-06-12 15:50:26 +0530

    ADDed BlogSlice

---
+++ clientside/src/features/blogs/blogSlice.js
@@ -0,0 +1,26 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    blogs: [],
+    blogData: {},
+};
+
+const blogSlice = createSlice({
+    name: 'blogs',
+    initialState,
+    reducers: {
+        setBlogs: (state, action) => {
+            state.blogs = action.payload.blogs;
+        },
+        setIndividualBlog: (state, action) => {
+            state.blogData = action.payload.blog;
+        },
+    } ,
+});
+
+export const {setBlogs, setIndividualBlog} = blogSlice.actions;
+
+export const AllBlogs = state => state.blogs.blogs;
+export const BlogData = state => state.blogs.blogData;
+
+export default blogSlice.reducer;
